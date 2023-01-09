8e3bd8f893c153f8539bcb607f5e1b5565b9c106
hajarabdulla <hajar.abdullayeva@code.edu.az>
2023-01-10 01:17:56 +0400

    style updated

--- app.js
+++ app.js
@@ -1,150 +1,158 @@
-const API = "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple";
-const startPage = document.querySelector(".start-page")
+const API =
+  "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple";
+const startPage = document.querySelector(".start-page");
 const startBtn = document.querySelector("#start-btn");
 
-const gameSection = document.querySelector(".game-section")
+const gameSection = document.querySelector(".game-section");
 const questionCounter = document.querySelector(".question-counter");
 const smallScale = document.querySelector(".small-scale");
 const score = document.querySelector(".score");
 const container = document.querySelector("#question-container");
-const inner = document.querySelector(".inner")
+const inner = document.querySelector(".inner");
 const question = document.querySelectorAll("#question");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
-
-questionCounter.innerHTML = `Question 0/10`;
 
 let start = 0;
 let scale = 0;
 const increment = 30;
 let length = 0;
 
+questionCounter.innerHTML = `${start + 1}/10`;
+
 startBtn.addEventListener("click", async () => {
-    startPage.style.display = "none";
-    gameSection.style.display = "block";
-    nextBtn.style.display = "block";
-    let questions = await getQuestions(API);
-    let shuffledQuestions = questions.map((question) => {
-        return {
-            ...question,
-            shuffledAnswers: shuffle([
-                question.correct_answer,
-                ...question.incorrect_answers,
-            ]),
-        };
-    });
-    createQuestionCard(shuffledQuestions);
+  startPage.style.display = "none";
+  gameSection.style.display = "block";
+  nextBtn.style.display = "block";
+  let questions = await getQuestions(API);
+  let shuffledQuestions = questions.map((question) => {
+    return {
+      ...question,
+      shuffledAnswers: shuffle([
+        question.correct_answer,
+        ...question.incorrect_answers,
+      ]),
+    };
+  });
+  createQuestionCard(shuffledQuestions);
 });
 
 //Get question
 async function getQuestions(api) {
-    let response = await fetch(api);
-    let result = await response.json();
-    return result.results;
+  let response = await fetch(api);
+  let result = await response.json();
+  return result.results;
 }
 
 // Game score
 function increaseScore() {
-    score.textContent = start;
+  score.textContent = start;
 }
 
 //Create question Card
 function createQuestionCard(arr) {
-    arr.map((question) => {
-        questionCounter.innerHTML = `Question ${start + 1}/10`;
+  arr.map((question) => {
+    // questionCounter.innerHTML = `${start + 1}/10`;
+    // start = start + 1;
 
-        const questionCard = document.createElement("div");
-        questionCard.id = "question";
+    const questionCard = document.createElement("div");
+    questionCard.id = "question";
 
-        const questionDescription = document.createElement("div")
-        questionDescription.classList.add("question-description")
+    const questionDescription = document.createElement("div");
+    questionDescription.classList.add("question-description");
 
-        const questionText = document.createElement("h3");
-        questionText.textContent = question.question;
+    const questionText = document.createElement("h3");
+    questionText.textContent = question.question;
 
-        const buttonGroup = document.createElement("div");
-        buttonGroup.classList.add("button-group");
+    const buttonGroup = document.createElement("div");
+    buttonGroup.classList.add("button-group");
 
-        question.shuffledAnswers.map((answer) => {
-            const button = document.createElement("button");
-            button.className = 'answer-button';
-            button.textContent = answer;
-            button.addEventListener("click", function () {
+    question.shuffledAnswers.map((answer) => {
+      const button = document.createElement("button");
+      button.className = "answer-button";
+      button.textContent = answer;
+      button.addEventListener("click", function () {
+        length++;
 
-                length++;
+        const p = button.parentElement.children;
 
-                const p = button.parentElement.children;
+        for (let i = 0; i < p.length; i++) {
+          p[i].disabled = true;
+        }
 
-                for (let i = 0; i < p.length; i++) {
-                    p[i].disabled = true;
-                }
+        if (answer === question.correct_answer) {
+          alert("true");
+          start++;
+          // scale = scale + increment
+          increaseScore();
+          this.style.background = "green";
+          this.style.color = "white";
+          scale += increment;
+          smallScale.style.width = scale + "px";
+          console.log("scale: ", scale);
+          console.log("Small Scale", smallScale);
+        } else {
+          this.style.background = "red";
+          this.style.color = "white";
+        }
 
-                if (answer === question.correct_answer) {
-                    alert("true");
-                    start++;
-                    // scale = scale + increment
-                    increaseScore();
-                    this.style.background = 'green';
-                    this.style.color = 'white';
-                    scale += increment;
-                    smallScale.style.width = scale + "px";
+        if (length >= 10) {
+          console.log(length);
+          alert(`your score is ${score}`);
+        }
+      });
 
-                } else {
-                    this.style.background = 'red';
-                    this.style.color = 'white';
-                }
-
-                if (length >= 10) {
-                    console.log(length)
-                    alert(`your score is ${score}`)
-                }
-            });
-
-            buttonGroup.appendChild(button);
-        });
-        questionDescription.appendChild(questionText)
-        questionCard.appendChild(questionDescription);
-        questionCard.appendChild(buttonGroup);
-        inner.appendChild(questionCard);
+      buttonGroup.appendChild(button);
     });
+    questionDescription.appendChild(questionText);
+    questionCard.appendChild(questionDescription);
+    questionCard.appendChild(buttonGroup);
+    inner.appendChild(questionCard);
+  });
 }
 
 //Shuffle array function
 function shuffle(array) {
-    let currentIndex = array.length,
-        randomIndex;
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex],
-            array[currentIndex],
-        ];
-    }
-    return array;
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
 }
 
 const height = parseInt(window.getComputedStyle(container).height);
 
+console.log("start: ", start);
+
 let offset = 0;
 nextBtn.addEventListener("click", () => {
-    if (offset === height * (question.length - 1)) {
-        offset = 0;
-        console.log(offset);
-    } else {
-        offset = offset + height - 20;
-    }
+  start = start + 1;
+  questionCounter.innerHTML = `${start + 1}/10`;
 
-    inner.style.transform = `translateY(-${offset}px)`
-})
+  if (offset === height * (question.length - 1)) {
+    offset = 0;
+    console.log(offset);
+  } else {
+    offset = offset + height - 20;
+  }
+
+  inner.style.transform = `translateY(-${offset}px)`;
+});
 
 prevBtn.addEventListener("click", () => {
-    if (offset === 0) {
-        offset = height * (question.length - 1);
-    } else {
-        offset = offset - height + 20;
-    }
+  if (offset === 0) {
+    offset = height * (question.length - 1);
+  } else {
+    offset = offset - height + 20;
+  }
 
-    inner.style.transform = `translateY(-${offset}px)`
-})
+  inner.style.transform = `translateY(-${offset}px)`;
+});
 
+console.log(scale);
